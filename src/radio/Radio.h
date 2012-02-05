a96c475c41ab89cb2626356a642679186509848b
Fredrik Brosser <fb.msngr@gmail.com>
2012-02-05 13:36:34 +0100

    Adding error codes and defines

--- src/radio/Radio.h
+++ src/radio/Radio.h
@@ -4,31 +4,33 @@
 #include <Xbee.h>
 #include <NewSoftSerial.h>
 
-#define ON = HIGH;
-#define OFF = LOW
+// Error codes
+# define unknownResponseFormat 3
+# define errorResponse 4
+
+#define ON HIGH
+#define OFF LOW
 
  // Delay time for LEDs (in ms)
-#define delayTime = 500;
+#define delayTime 500
 
 // LED Pin definitions
-#define statusLed = 11;
-#define errorLed = 12;
-#define dataLed = 10;
+#define statusLed 11
+#define errorLed 12
+#define dataLed 10
 
 class Radio {
-public:
-	 // Default constructor
-	Radio();
-
-	void displayError(uint8_t errorCode);
-
-private:
-	uint8_t option;
-	uint8_t data;
-	bool dFlag;
-	XBee xbee;
-	XBeeResponse response;
-	Rx16Response rx16;
-	Rx64Response rx64 ;
-
+	
+	public:
+		 // Default constructor
+		Radio();
+		void displayError(uint8_t errorCode);
+	private:
+		uint8_t option;
+		uint8_t data;
+		bool dFlag;
+		XBee xbee;
+		XBeeResponse response;
+		Rx16Response rx16;
+		Rx64Response rx64 ;
 };
