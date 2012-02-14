60160ddf401483c4ade705c1faf4354d4fef71e9
Fredrik Brosser <fb.msngr@gmail.com>
2012-02-14 15:30:44 +0100

    Starting work on radio class

--- src/radio/Radio.h
+++ src/radio/Radio.h
@@ -1,36 +1,32 @@
 #ifndef Radio_h
 #define Radio_h
 
+// Character used for modem pair-up
+#define joinCharacter ('H')
+// Baud rate for ZigBees
+#define baudrate 9600
+
 #include <Xbee.h>
 #include <NewSoftSerial.h>
-
-// Error codes
-# define unknownResponseFormat 3
-# define errorResponse 4
-
-#define ON HIGH
-#define OFF LOW
-
- // Delay time for LEDs (in ms)
-#define delayTime 500
-
-// LED Pin definitions
-#define statusLed 11
-#define errorLed 12
-#define dataLed 10
 
 class Radio {
 	
 	public:
-		 // Default constructor
-		Radio();
-		void displayError(uint8_t errorCode);
+		// Default constructor
+		Radio(HardwareSerial* serialPort);
+		// Used by End Device to find and join network
+		void findPANCoordinator();
+		// Used by Coordinator to listen for End Devices
+		void permitJoining();
+		// Puts modem into control mode, accepting commands
+		void enterCtrlMode();
+		// Enable/Disable low power mode ('sleep')
+		void setSleepMode(bool sleep);
+		// Send string via serial port
+		void send(char *charPtr);
+		// Listen on serial port
+		char* receive();
 	private:
-		uint8_t option;
-		uint8_t data;
-		bool dFlag;
-		XBee xbee;
-		XBeeResponse response;
-		Rx16Response rx16;
-		Rx64Response rx64 ;
+		char* rxBuffer;
+		HardwareSerial* serialPort;
 };
