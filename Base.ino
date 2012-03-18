f5a3f1dcc39d1d784fca199d150e2838035093f6
Kim Burgestrand <kim@burgestrand.se>
2012-03-18 16:37:56 +0100

    Add Utilities.h/Utilities.cpp

--- Base.ino
+++ Base.ino
@@ -7,5 +7,5 @@ void setup(void)
 
 void loop(void)
 {
-
+  
 }
--- Common/Utilities.h
+++ Common/Utilities.h
@@ -4,13 +4,6 @@
 /* Required includes */
 #include <string.h> /* for size_t */
 
-#ifdef ARDUINO
-#  include "arduino_helpers.h"
-#  define show(fmt, ...) Serial.println(fmt)
-#else
-#  define show(fmt, ...) printf(fmt "\n", __VA_ARGS__)
-#endif
-
 /* Macros */
 #define true 1
 #define false 0
@@ -18,7 +11,6 @@
 #define ALLOC(type) ALLOC_N(type, 1)
 #define ALLOC_N(type, n) ((type*) xmalloc(sizeof(type) * (n)))
 #define ALLOC_STR(n) ALLOC_N(char, n + 1)
-#define ALLOC_USTR(n) (unsigned char *)ALLOC_STR(n)
 
 #define MEMCPY(dst, src, type) MEMCPY_N(dst, src, type, 1)
 #define MEMCPY_N(dst, src, type, n) (memcpy((dst), (src), sizeof(type) * (n)))
