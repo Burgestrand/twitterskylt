4f1f07fa4e1d40ed05f4bf3bb7198311e6e6aa3d
andreas <ake.andreas@gmail.com>
2012-03-20 09:15:13 +0100

    added modified ethernet lib

---
+++ src/ethernet_new/Ethernet.cpp
@@ -0,0 +1,121 @@
+#include "w5100.h"
+#include "Ethernet.h"
+#include "Dhcp.h"
+
+// XXX: don't make assumptions about the value of MAX_SOCK_NUM.
+uint8_t EthernetClass::_state[MAX_SOCK_NUM] = { 
+  0, 0, 0, 0 };
+uint16_t EthernetClass::_server_port[MAX_SOCK_NUM] = { 
+  0, 0, 0, 0 };
+;
+
+int EthernetClass::begin(uint8_t *mac_address)
+{
+
+  // Initialise the basic info
+  W5100.init();
+  W5100.setMACAddress(mac_address);
+  // moved set if ip 0.0.0.0 to dhcp lib
+  //W5100.setIPAddress(IPAddress(0,0,0,0).raw_address());
+
+  // Now try to get our config info from a DHCP server
+  int ret = _dhcp.beginWithDHCP(mac_address);
+  if(ret == 1)
+  {
+    // We've successfully found a DHCP server and got our configuration info, so set things
+    // accordingly
+    setIpInfo();
+  }
+
+  return ret;
+}
+
+void EthernetClass::setIpInfo() {
+    // We've successfully found a DHCP server and got our configuration info, so set things
+    // accordingly
+    W5100.setIPAddress(_dhcp.getLocalIp().raw_address());
+    W5100.setGatewayIp(_dhcp.getGatewayIp().raw_address());
+    W5100.setSubnetMask(_dhcp.getSubnetMask().raw_address());
+    _dnsServerAddress = _dhcp.getDnsServerIp();
+}
+
+void EthernetClass::begin(uint8_t *mac_address, IPAddress local_ip)
+{
+  // Assume the DNS server will be the machine on the same network as the local IP
+  // but with last octet being '1'
+  IPAddress dns_server = local_ip;
+  dns_server[3] = 1;
+  begin(mac_address, local_ip, dns_server);
+}
+
+void EthernetClass::begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server)
+{
+  // Assume the gateway will be the machine on the same network as the local IP
+  // but with last octet being '1'
+  IPAddress gateway = local_ip;
+  gateway[3] = 1;
+  begin(mac_address, local_ip, dns_server, gateway);
+}
+
+void EthernetClass::begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway)
+{
+  IPAddress subnet(255, 255, 255, 0);
+  begin(mac_address, local_ip, dns_server, gateway, subnet);
+}
+
+void EthernetClass::begin(uint8_t *mac, IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet)
+{
+  W5100.init();
+  W5100.setMACAddress(mac);
+  W5100.setIPAddress(local_ip._address);
+  W5100.setGatewayIp(gateway._address);
+  W5100.setSubnetMask(subnet._address);
+  _dnsServerAddress = dns_server;
+}
+
+// returns 0 if ok
+int EthernetClass::renew()
+{
+
+  int ret = _dhcp.renew();
+
+  if (ret == 1) {
+    setIpInfo();
+    return 0;
+  }
+
+  else if (ret == 2)
+    return 0;
+
+  else
+    return 1;
+    
+}
+
+IPAddress EthernetClass::localIP()
+{
+  IPAddress ret;
+  W5100.getIPAddress(ret.raw_address());
+  return ret;
+}
+
+IPAddress EthernetClass::subnetMask()
+{
+  IPAddress ret;
+  W5100.getSubnetMask(ret.raw_address());
+  return ret;
+}
+
+IPAddress EthernetClass::gatewayIP()
+{
+  IPAddress ret;
+  W5100.getGatewayIp(ret.raw_address());
+  return ret;
+}
+
+IPAddress EthernetClass::dnsServerIP()
+{
+  return _dnsServerAddress;
+}
+
+EthernetClass Ethernet;
