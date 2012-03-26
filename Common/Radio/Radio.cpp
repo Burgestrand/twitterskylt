ccecb14996f2321d7bc42aab6f8ef548e5694d8a
Fredrik Brosser <fb.msngr@gmail.com>
2012-03-26 11:58:37 +0200

    Add radio parent class

---
+++ Common/Radio/Radio.cpp
@@ -0,0 +1,28 @@
+#include "Radio.h"
+
+Radio::Radio() {
+	// Default serial port
+	// this->serialPort = Serial;
+}
+
+void Radio::begin(HardwareSerial &serialPort) {
+	// Initialize XBee
+	this->xbee = XBee();
+	// Set XBee serial port 
+	this->xbee.setSerial(serialPort);
+	// Set XBee Baud Rate
+	this->xbee.begin(BAUDRATE);
+}
+
+void Radio::send(uint8_t *payload, uint8_t payloadLength) {
+	xbee.reset();
+	XBeeAddress64 destAddr64_n = XBeeAddress64(0x00000000, 0x0000FFFF);
+	zbTx = ZBTxRequest(destAddr64_n, payload, payloadLength);
+	xbee.send(zbTx);
+}
+
+void Radio::sendATCommand(uint8_t *cmd) {
+	xbee.reset();
+	AtCommandRequest atRequest = AtCommandRequest(cmd);
+  	xbee.send(atRequest);
+}
