cb4ef047a6ee6cf4eacd47f48b63995ab61b8e3f
andreas <ake.andreas@gmail.com>
2012-03-27 11:44:23 +0200

    updated SD lib to new requiresments

--- src/AccConfig/AccConfig.cpp
+++ src/AccConfig/AccConfig.cpp
@@ -17,78 +17,147 @@ when there is no more hashtags hashtags[i] is null
 
 #include "AccConfig.h"
 
-
-char ** indata;
-
-AccConfig::AccConfig(char * configFile) {
+int AccConfig::begin(char * configFile) {
+ 
+  buf = NULL; 
   
   // Inits the SD shield
   pinMode(53, OUTPUT);
-  SD.begin(4);
+
+  if (!SD.begin(4)) {
+    //SD.begin failed
+    return 3;
+  }
+
+  if (!SD.exists(configFile)) {
+    // file don't exists
+    return 5;
+  }
 
   File f = SD.open(configFile);
   
+  if (!f) {
+    // couldn't open file
+    return 1;
+  }
+  
+  unsigned long file_size = f.size();
+  
+  buf = (char *) malloc(sizeof(char) * (file_size + 1));
+
+  if (buf == NULL) {
+    //couln't allocate memory
+    return 4;
+  }
+  
   char tmp;
-  char *pek = sd;
-  
-  int in_size = 0;
-  int i = 0;
-  int x;
-  
-  int num = 4;
-  indata = (char **) malloc(sizeof(char *)*num);
+  char * pek = buf;
+
+  /*
+    whitespace condition
+    0: start, before the non-whitespace
+    1: the username
+    2: whitespace after the username
+    3: a char belongs to the query
+   */
+  int wscond = 0;
 
   while(f.available()) {
     tmp = (char) f.read();
+    
+    switch (wscond) {
+  
+    case 0:
 
-    // check if it's a charachter then store it in buffer sd
-    if (!(tmp == '\n' || tmp == ' ' || tmp == '\t' || tmp == '#')) {
+      if (!(pek == buf && (tmp == '\n' || tmp == ' ' || tmp == '\t'))) {
+	*pek = tmp;
+	pek++;
+	wscond = 1;
+      }
+      break;
+      
+    case 1:
+
+      if (tmp == '\n' || tmp == ' ' || tmp == '\t') {
+	*pek = '\0';
+	pek++;
+	wscond = 2;
+      }
+      else {
+	*pek = tmp;
+	pek++;
+      }
+      break;
+
+    case 2:
+
+      if (!(tmp == '\n' || tmp == ' ' || tmp == '\t')) {
+	*pek = tmp;
+	pek++;
+	wscond = 3;
+      }
+      break;
+
+    default:
       *pek = tmp;
-      in_size++;
       pek++;
     }
-    // the word is done
-    else if (in_size > 0) {
-      
-      // no more space so allocate some more
-      if (i == num-1) {
-	num += 4;
-	char ** newcopy = (char **) malloc(sizeof(char *) * num);
-
-	// move pointers to the new array
-	for (x = 0; x < num-4; x++)
-	  newcopy[x] = indata[x];
-
-	// deallocate the old space 
-	free(indata);
-	indata = newcopy;
-      }
-
-      char * config = (char *) malloc(sizeof(char) * (in_size+1));
-      indata[i] = config;
-      
-      // move data from buffer sd to the array
-      for (x = 0; x < in_size; x++)
-	config[x] = sd[x];
-
-      config[x] = '\0';
-      pek = sd;
-      in_size = 0;
-      i++;
-    }
-    // else multiple whitespaces, ignore sign and loop again
   }
   
-  // insert null so we know theres no more relevant data
-  indata[i] = '\0';
-  f.close();
+
+  if (wscond == 0) {
+    // no username in file
+    buf = NULL;
+    return 2;
+  }
   
+  // loops while theres no space at the end of query
+  do {
+    pek--;
+  } while(*pek < 33);
+  
+  // adds a null char at end
+  pek++;
+  *pek = '\0';
+
+  int diff = (pek - buf) + 1; // have to include all numbers
+  Serial.println("file: ");
+  Serial.println(file_size);
+  Serial.println("diff: ");
+  Serial.println(diff);
+  if (file_size != diff) {
+    Serial.println("realloc");
+    char * new_ptr = (char *) realloc(buf, diff);
+    if (new_ptr != NULL)
+      buf = new_ptr;
+    else {
+      // couldn't allocate more memory
+      return 4;
+    }
+  }
+  
+  if (wscond == 2) {
+    // no query so pointer is null
+    query = '\0';
+  }
+  else {
+    pek = buf;
+    while (*pek != '\0')
+      pek++;
+    pek++;
+    query = pek;
+  }
+
+  f.close();
+  return 0; // everything was ok
 }
 
+// returns null pointer if no username found
 char * AccConfig::getUsername() {
-  return indata[0];
+  return buf;
 }
 
-char ** AccConfig::getHashtags() {
-  return indata + 1;
+// returns null pointer if no query found
+char * AccConfig::getQuery() {
+  return query;
 }
