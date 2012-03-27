e0558aa1a431c8c64c17940eef995141a77fe347
Jakob Kallin <Jakob@jakob-laptop.(none)>
2012-03-27 21:23:26 +0200

    Organize all formatting code into files and remove dependency on helpers.

---
+++ Base/Encoding.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+
+namespace Formatting
+{
+	char *utf8_strip(const char *dirty);
+}
---
+++ Base/Normalization.cpp
@@ -0,0 +1,82 @@
+#include "Normalization.h"
+
+namespace Formatting
+{
+	static char *strclone(const char *string)
+	{
+		char *clone = NULL;
+
+		if (string == NULL)
+		{
+			return NULL;
+		}
+
+		clone = (char *) calloc(strlen(string) + 1, sizeof(char));
+		strcpy(clone, string);
+		return clone;
+	}
+	
+	static int matches(char byte, char *tokens)
+	{
+		char *token = NULL;
+
+		for (token = tokens; *token != '\0'; ++token)
+		{
+			if (*token == byte)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	char **strsplit(const char *_string, int *num_words)
+	{
+		char **words = NULL;
+		char *string = strclone(_string);
+		char *cursor = string;
+		char *tokens = " ";
+
+		if (_string == NULL)
+		{
+			return words;
+		}
+
+		/* first, we count number of words */
+		*num_words = 0;
+		int on_word	= false;
+		int on_whitespace = false;
+
+		for (cursor = string; *cursor != '\0'; ++cursor)
+		{
+			on_whitespace = matches(*cursor, tokens);
+
+			if (on_whitespace && on_word)
+			{
+				on_word = false;
+			}
+			else if ( ! on_whitespace && ! on_word)
+			{
+				(*num_words)++;
+				on_word = true;
+			}
+		}
+
+		/* allocate space for all words */
+		words	= calloc(*num_words, sizeof(char *));
+		cursor = strtok(string, tokens);
+		int i	= 0;
+
+		while (cursor != NULL)
+		{
+			words[i++] = strclone(cursor);
+			cursor = strtok(NULL, tokens);
+		}
+
+		/* free resources */
+		free(string);
+
+		return words;
+	}
+}
