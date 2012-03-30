30127875dba1be9dc2bbbcbcb1420f4879faa0cb
Lars Tidstam <larstidstam@gmail.com>
2012-03-30 11:58:34 +0200

    Moved Display module to new layout.

--- Sign/Display/Display.cpp
+++ Sign/Display/Display.cpp
@@ -9,22 +9,27 @@
 #include "Display.h"
 
 
+// Constructor defining the pins used by the display.
 Display::Display(uint8_t rs, uint8_t e, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) : _disp(rs, e, d4, d5, d6, d7) {
-//	_disp(rs, e, d4, d5, d6, d7);
 	_row = 0;
 	_done = 0;
 }
 
+// Initialize and clear display.
 void Display::begin(uint8_t col, uint8_t row) {
 	_disp.begin(col, row);
 	_disp.clear();
 }
 
+// Set the cursor to the start of the next line, wraps around to the first line if needed.
 void Display::new_line() {
-	_row = (_row + 1) % 2;
+	_row = (_row + 1) % 2; // XXX: Hard coded number of lines!
 	_disp.setCursor(0, _row);
 }
 
+// Write a char, handles newlines and end of message characters
+// If a end of message character has been encountered, the next 
+// time a char is printed the display is cleared before outputting it.
 void Display::write(uint8_t c) {
 	if (_done) {
 		clear();
@@ -44,12 +49,15 @@ void Display::write(uint8_t c) {
 	}
 }
 
-void Display::write(String str) {
-	for(int i=0; i<str.length(); i++) {
-		write(str.charAt(i));
+// Null terminate the string, or else!
+void Display::write(char * msg) {
+	while (*msg != '\0') {
+		write(*msg);
+		msg++;
 	}
 }
 
+// Clear the display.
 void Display::clear() {
 	_disp.clear();
 	_row = 0;
