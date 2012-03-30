65b5d85581259ba04d47f2020f67c75c8ab22686
andreas <ake.andreas@gmail.com>
2012-03-30 15:09:17 +0200

    changed schetch

--- Base/Base.ino
+++ Base/Base.ino
@@ -1,7 +1,12 @@
-#include "Common/Common.h"
-#include "Coordinator/Coordinator.h"
-#include "Ethernet/Ethernet.h"
-#include "HTTP.h"
+#include <Radio.h>
+#include <Coordinator.h>
+//#include <Ethernet.h>
+#include <HTTP.h>
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+#include <XBee.h>
+#include <Formatting.h>
+#include <Time.h>
 
 Coordinator coordinator;
 
@@ -9,12 +14,12 @@ void setup(void)
 {
 	coordinator.begin(Serial1);
 	byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xF9, 0x83 };
-	Ethernet.begin(mac);
+	//Ethernet.begin(mac);
 }
 
 void loop(void)
 {
-    Ethernet.renew();
+    //Ethernet.renew();
     coordinator.tick();
-    HTTP.tick(coordinator);
+    HTTP::tick(coordinator);
 }
--- Base/HTTP.cpp
+++ Base/HTTP.cpp
@@ -1,16 +1,21 @@
+#include "HTTP.h"
+
 namespace HTTP
 {
-	static done = false;
+	static boolean done = false;
 	
-	uint8_t tick()
+	uint8_t tick(Coordinator coordinator)
 	{
 		if (!done)
 		{
-			char tweet[] = "charlie sheen roxx";
-			char twitter_date[] = "Fri, 30 Mar 2012 23:23:07 +0000";
-			int utc_offset = 3600;
-			char message = Formatting::format(tweet, twitter_date, utc_offset);
-			Coordinator.setData(message, strlen(message));
+			//char tweet[] = "charlie sheen roxx";
+			//char twitter_date[] = "Fri, 30 Mar 2012 23:23:07 +0000";
+			//int utc_offset = 3600;
+			//uint8_t * message = (uint8_t *)Formatting::format(tweet, twitter_date, utc_offset);
+			//uint8_t message[] = {'a', 'b'};
+			uint8_t * testdata = NULL;
+			//coordinator.setData(message, sizeof(message));
+			coordinator.setData(testdata, 5);
 			done = true;
 		}
 		
