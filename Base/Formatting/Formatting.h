fb20deeaa785f1f30528323e68aa97bfb9cc1142
andreas <ake.andreas@gmail.com>
2012-03-30 14:23:37 +0200

    change structure

---
+++ Base/Formatting/Formatting.h
@@ -0,0 +1,9 @@
+#include "Encoding.h"
+#include "Normalization.h"
+#include "Justification.h"
+#include "Metadata.h"
+
+namespace Formatting
+{
+	char *format(char *message, char *twitter_date, int utc_offset);
+}
---
+++ Base/Formatting/Justification.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "Constants.h"
+#include "Arduino.h"
+
+namespace Formatting
+{
+	char *justify(char **original_words, uint8_t word_count);
+}
