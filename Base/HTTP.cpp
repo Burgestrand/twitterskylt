059f658e547b1a44e689af8b913de152c0ebffc2
Jakob Kallin <Jakob@jakob-laptop.(none)>
2012-03-30 11:51:20 +0200

    Set data of Coordinator, not Radio.

--- Base/HTTP.cpp
+++ Base/HTTP.cpp
@@ -10,7 +10,7 @@ namespace HTTP
 			char twitter_date[] = "Fri, 30 Mar 2012 23:23:07 +0000";
 			int utc_offset = 3600;
 			char message = Formatting::format(tweet, twitter_date, utc_offset);
-			Radio.setData(message, strlen(message));
+			Coordinator.setData(message, strlen(message));
 			done = true;
 		}
 		
