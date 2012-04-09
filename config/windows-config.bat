ed576d48752d4e468047d8b1d2b20f67bcb62c03
Jakob Kallin <Jakob@jakob-laptop.(none)>
2012-04-09 17:12:20 +0200

    Remove unnecessary parentheses.

--- config/windows-config.bat
+++ config/windows-config.bat
@@ -3,10 +3,10 @@ setlocal
 set CONFIG_FILE=config.txt
 
 echo Enter your Twitter username:
-(set /p TWITTER_USERNAME=)
+set /p TWITTER_USERNAME=
 
 echo Enter search query to filter tweets with. (Leave blank to receive all tweets.)
-(set /p TWITTER_QUERY=)
+set /p TWITTER_QUERY=
 set TWITTER_QUERY=from:%TWITTER_USERNAME% AND (%TWITTER_QUERY%)
 
 echo Your settings have been saved. To change your settings, run this script again.
