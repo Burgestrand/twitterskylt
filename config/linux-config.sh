7dbad7819c8d2bbfddd697f084e4981bbff0fa0c
Jakob Kallin <Jakob@jakob-laptop.(none)>
2012-04-10 11:18:18 +0200

    Do not add custom query if it is blank.

--- config/linux-config.sh
+++ config/linux-config.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-CONFIG_FILE=config.txt
+TWITTER_CONFIG_FILE=config.txt
 
 echo "Enter your Twitter username: "
 read TWITTER_USERNAME
+TWITTER_QUERY="from:$TWITTER_USERNAME"
 
 echo "Enter search query to filter tweets with. (Leave blank to receive all tweets.)"
-read TWITTER_QUERY
-TWITTER_QUERY="from:$TWITTER_USERNAME AND ($TWITTER_QUERY)"
+read CUSTOM_TWITTER_QUERY
+if [[ $CUSTOM_TWITTER_QUERY != "" ]]
+	then TWITTER_QUERY="$TWITTER_QUERY AND ($CUSTOM_TWITTER_QUERY)"
+fi
 
 echo "Your settings have been saved. To change your settings, run this script again."
 
-echo $TWITTER_USERNAME > $CONFIG_FILE
-echo $TWITTER_QUERY >> $CONFIG_FILE
+echo $TWITTER_USERNAME > $TWITTER_CONFIG_FILE
+echo $TWITTER_QUERY >> $TWITTER_CONFIG_FILE
--- config/windows-config.bat
+++ config/windows-config.bat
@@ -3,15 +3,16 @@ setlocal
 set CONFIG_FILE=config.txt
 
 echo Enter your Twitter username:
-set /p TWITTER_USERNAME=
+set /p USERNAME=
+set QUERY=from:%USERNAME%
 
 echo Enter search query to filter tweets with. (Leave blank to receive all tweets.)
-set /p TWITTER_QUERY=
-set TWITTER_QUERY=from:%TWITTER_USERNAME% AND (%TWITTER_QUERY%)
+set /p CUSTOM_QUERY=
+if defined CUSTOM_QUERY set QUERY=%QUERY% AND (%CUSTOM_QUERY%)
 
 echo Your settings have been saved. To change your settings, run this script again.
 
-echo %TWITTER_USERNAME%>%CONFIG_FILE%
-echo %TWITTER_QUERY%>>%CONFIG_FILE%
+echo %USERNAME%>%CONFIG_FILE%
+echo %QUERY%>>%CONFIG_FILE%
 
 endlocal
