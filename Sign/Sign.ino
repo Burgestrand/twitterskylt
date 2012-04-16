c4c13e5e0d90b545d73c4e9f709ced58bed38d83
Lars Tidstam <larstidstam@gmail.com>
2012-04-16 13:17:30 +0200

    Remove unused code/variables, update comments in sign sketch.

--- Sign/Sign.ino
+++ Sign/Sign.ino
@@ -1,5 +1,4 @@
 
-// #include "Common/Common.h" /* Not needed? */
 #include <avr/interrupt.h>
 #include <XBee.h>
 #include <SoftwareSerial.h>
@@ -9,17 +8,16 @@
 #include <Sleep.h>
 
 // TODO: 
-// Debouncing update/join buttons
-// Handle xbee SLEEP pins better.
-// xbee error handling!
+// Debouncing update/join buttons?
+// Radio error handling
 
-// PINS /////////////////////////////////////////////////////////////////////////////////
+// Pins
 // Display related
 #define DISP_RS 10
 #define DISP_EN 11
 #define DISP_DATA 4
 
-// Software serial for debugging
+// Software serial pins for debugging
 #define SERIAL_RX 9
 #define SERIAL_TX 8
 
@@ -30,16 +28,16 @@
 #define LED_2 A1
 
 // Buttons
-// The one closest to the leds
+// The button closest to the leds
 #define BUTTON_1 2
-// The one furthest from the leds
+// The button furthest from the leds
 #define BUTTON_2 3
 
 // XBee sleep request/status
 #define SLEEP_RQ_PIN 12
 #define SLEEP_STATUS_PIN 13
 
-// GLOBAL VARIABLES ////////////////////////////////////////////////////////////////
+// Global variables
 // The display
 Display disp(DISP_RS, DISP_EN, DISP_DATA, DISP_DATA+1, DISP_DATA+2, DISP_DATA+3);
 
@@ -49,17 +47,13 @@ EndDevice radio(SLEEP_RQ_PIN, SLEEP_STATUS_PIN);
 // The serial connection used for debug output
 SoftwareSerial ss(SERIAL_RX, SERIAL_TX);
 
-// State for the button debounce
-long latestJoinDown;
-long latestUpdateDown;
-
 // True if the join button has been pressed, but not yet handled
 volatile bool joinPressed = false;
 
 // True if the update button has been pressed, but not yet handled
 volatile bool updatePressed = false;
 
-// HELPER FUNCTIONS /////////////////////////////////////////////////////////////////
+// Helpers
 // Debug output callback
 void debug(char *msg) {
   ss.write("DEBUG: ");
@@ -77,9 +71,9 @@ void updateButtonPressed(void) {
   updatePressed = true;
 }
 
-// REGULAR ARDUINO STUFF /////////////////////////////////////////////////////////////
+// Usual arduino functions
 void setup () {
-  // Activity  led indicating that the loop runs
+  // Activity  led indicating that the arduino is awake
   pinMode(LED_1, OUTPUT);
   digitalWrite(LED_1, HIGH);
   
@@ -89,14 +83,10 @@ void setup () {
   // Initialize debug serial port
   ss.begin(9600);
   
-  // Setup update button and SW debounce
-  latestJoinDown = millis();
-  latestUpdateDown = millis();
-  
   // Initialize the radio
   radio.begin(9600);
   
-  // Initialize sleep timer
+  // Initialize the sleep timer
   sleep_begin();
   
   // Setup the buttons
@@ -108,8 +98,7 @@ void setup () {
 }
 
 void loop () {
-  // Handle button presses, with debounce.
-  /*uint8_t down;*/
+  // Handle button presses
   // Pair button
   if (joinPressed) {
     joinPressed = false;
@@ -117,38 +106,13 @@ void loop () {
     disp.write("Trying to join\nnetwork\004");
     radio.joinNetwork();
   }
+  // Update button
   if (updatePressed) {
     updatePressed = false;
     debug("UPDATEBTN NOTICED");
     disp.write("Updating...\004");
     radio.getNewestMessage();
   }
-  /*noInterrupts();
-  down = joinPressed;
-  joinPressed = false;
-  interrupts();
-  if (down && latestJoinDown < millis() - 500) {
-    debug("JOINBTN NOTICED");
-    disp.write("Trying to join\nnetwork\004");
-    radio.joinNetwork();
-  }
-  if (down) {
-    latestJoinDown = millis();
-  }*/
-  
-  // Update button
-  /*noInterrupts();
-  down = updatePressed;
-  updatePressed = false;
-  interrupts();
-  if (down && latestUpdateDown < millis() - 500) {
-    debug("UPDATEBTN NOTICED");
-    disp.write("Updating...\004");
-    radio.getNewestMessage();
-  }
-  if (down) {
-    latestUpdateDown = millis();
-  }*/
   
   // Necessary for the radio library to work
   switch (radio.tick()) {
@@ -158,7 +122,7 @@ void loop () {
       break;
     case TICK_JOIN_NOT_DELIVERED:
       // Could not deliver join message
-      disp.write("Join:\n No deliver\004");
+      disp.write("Join:\n No delivery\004");
       break;
     case TICK_JOIN_TIMEOUT:
       disp.write("Join:\n Timeout\004");
@@ -167,10 +131,10 @@ void loop () {
       disp.write("Joined!\004");
       break;
     case TICK_UPDATE_NO_DELIVERY:
-      disp.write("Update:\nNo deliver\004");
+      disp.write("Update:\n No delivery\004");
       break;
     case TICK_UPDATE_TIMEOUT:
-      disp.write("Update:\nTimeout\004");
+      disp.write("Update:\n Timeout\004");
       break;
     case TICK_NEW_MSG:
       disp.write((char *) radio.getData());
@@ -178,8 +142,7 @@ void loop () {
       debug("GOT NEW MSG");
       break;
     case TICK_SLEEPING:
-      //disp.write("Sleeping\004");
-      // Radio is sleeping, and we have nothing to do; lets sleep!
+      // Radio is sleeping and we have nothing to do; lets sleep!
       digitalWrite(LED_1, LOW);
       {
         uint8_t sleepRounds = 3;
