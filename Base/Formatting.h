9a6064f5227126c7dd3f5367ea49c6a56fe6ce8f
andreas <ake.andreas@gmail.com>
2012-04-19 11:44:21 +0200

    cleaned base dir

--- Base/Formatting.h
+++
@@ -1,9 +0,0 @@
-#include "Encoding.h"
-#include "Normalization.h"
-#include "Justification.h"
-#include "Metadata.h"
-
-namespace Formatting
-{
-	char *format(char *message, char *twitter_date, int utc_offset);
-}
--- Base/Justification.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-#include "Constants.h"
-
-namespace Formatting
-{
-	char *justify(char **original_words, uint8_t word_count);
-}
