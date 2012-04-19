bad325fd0f7a5f13fffed1488b29a6c37cc045b2
andreas <ake.andreas@gmail.com>
2012-04-19 16:50:56 +0200

    added errormsg and parsing

--- Base/Base.ino
+++ Base/Base.ino
@@ -11,6 +11,11 @@
 #include <Formatting.h>
 #include <Time.h>
 #include <SPI.h>
+#include <Ethernet.h>
+#include <AccConfig.h>
+#include <SD.h>
+#include <Common.h>
+#include <TweetParser.h>
 
 Coordinator coordinator;
 
@@ -21,45 +26,88 @@ unsigned long int tempTimer = 0;
 boolean assocPinStatus = false;
 SoftwareSerial nss = SoftwareSerial(30, 32);
 uint8_t sendData[120];
+AccConfig config;
 
 
 void setup(void)
 {
+	Serial.begin(9600);
+	pinMode(errorPin, OUTPUT);
+	pinMode(assocPin, OUTPUT);
 for(int i=0; i<72; i++) sendData[i] = '1';
 for(int i=72; i<120; i++) sendData[i] = '2';
 	coordinator.begin(Serial1);
+
+	int configStatus = config.begin("KONF1.TXT");
+	if (configStatus > 0) {
+		signalError(true);
+		abort();
+	}
+	Serial.println(config.getUsername());
+	Serial.println(config.getUsername());
+	Serial.println(config.getQuery());
+	Serial.println(config.getQuery());
 	byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xF9, 0x83 };
-	//Ethernet.begin(mac);
+	int ethernetStatus = Ethernet.begin(mac);
+	if (ethernetStatus > 0) {
+		signalError(true);
+		abort();
+	}
 	  attachInterrupt(0, pairUp, FALLING);   
 	  nss.begin(9600);
 	  nss.println(" ");
 	  nss.println(" ");
 	  nss.println("***** RESET *****");
 	  nss.println(" ");
-	  pinMode(errorPin, OUTPUT);
-	  pinMode(assocPin, OUTPUT);
-	  digitalWrite(errorPin, LOW);
-	  digitalWrite(assocPin, LOW);
+		signalError(false);
+		signalAssoc(false);	  
 	  blinkTimer = 0;
 	//coordinator.setData(sendData, sizeof(sendData));
 }
 
 void loop(void)
 {
-  HTTP http();
-    //Ethernet.renew();
+	//uint8_t ip[4] = {192, 168, 0, 1};
+  //HTTP http(ip, 10);
+  Ethernet.renew();
   delay(10);
   uint8_t st = coordinator.tick();
   uint8_t s = coordinator.getState();
   assocLed(s);
   printState(s);
   if(st == 1 || st == 3) {
-    digitalWrite(errorPin, HIGH);
+	signalError(true);    
+	//digitalWrite(errorPin, HIGH);
   }
   else {
-     digitalWrite(errorPin, LOW); 
+	signalError(false);
+     //digitalWrite(errorPin, LOW); 
   }
+
+	boolean parse_complete = false;
+
+	char json[] = {"{\"query\": \"from%3Aajstream+%23egypt\","
+					"\"results\": [{\"created_at\": \"Mon, 05 Mar 2012 01:00:16 +0000\","
+									"\"metadata\": {\"result_type\": \"recent\"},"
+									"\"text\": \"Protests at the German University of #Cairo #GUC #Egypt - http://t.co/PeZryCQR\"}],"
+					"\"results_per_page\": 1}"};
+
+	char * shared_buffer = (char *) malloc(256 * sizeof(char));
+	char * text = (char *) malloc(160 * sizeof(char));
+	char * date = (char *) malloc(32 * sizeof(char));
+	TweetParser parser(shared_buffer, text, 160, date, 32);
+	int json_start = 0;
+	while (!parse_complete) {
+		put_in_buf(json_start, 30, json, shared_buffer);
+		parse_complete = parser.parse(256); 
+	}
+
 }
+
+void put_in_buf(int begin, int length, char from[], char * to) {
+	for (int i = begin; i < length; i++)
+		to[i] = from[i];
+} 
 
 void assocLed(uint8_t state) {
   if(state == 7) {
@@ -67,10 +115,12 @@ void assocLed(uint8_t state) {
   }
   else {
     if(coordinator.getAssoc()) {
-      digitalWrite(assocPin, HIGH);
+		signalAssoc(true);      
+		//digitalWrite(assocPin, HIGH);
     }
     else {
-       digitalWrite(assocPin, LOW);
+		signalAssoc(false);
+       //digitalWrite(assocPin, LOW);
     }
   }  
 }
@@ -92,5 +142,23 @@ void printState(uint8_t s) {
   nss.print("]  =  [");
   nss.print(coordinator.getStateName(s));
   nss.println("]");
+}
+
+void signalError(boolean on) {
+	if (on)
+		digitalWrite(errorPin, HIGH);
+	else
+		digitalWrite(errorPin, LOW);
+}
+
+void signalAssoc(boolean on) {
+	if (on)
+		digitalWrite(assocPin, HIGH);
+	else
+		digitalWrite(assocPin, LOW);
+}
+
+void abort() {
+	for (;;);
 }
 
