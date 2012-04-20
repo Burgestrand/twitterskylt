a4ff542d31ff4fc4d8fbeb2d9b3c0495f4f428ec
Jakob Kallin <Jakob@jakob-laptop.(none)>
2012-04-20 13:45:44 +0200

    Clean up Base sketch.

--- Base/Base.ino
+++ Base/Base.ino
@@ -24,95 +24,55 @@ const int assocPin = 24;
 unsigned long int blinkTimer = 0;
 unsigned long int tempTimer = 0;
 boolean assocPinStatus = false;
-SoftwareSerial nss = SoftwareSerial(30, 32);
-uint8_t sendData[120];
 AccConfig config;
-
 
 void setup(void)
 {
 	Serial.begin(9600);
 	pinMode(errorPin, OUTPUT);
 	pinMode(assocPin, OUTPUT);
-for(int i=0; i<72; i++) sendData[i] = '1';
-for(int i=72; i<120; i++) sendData[i] = '2';
 	coordinator.begin(Serial1);
 
 	int configStatus = config.begin("KONF1.TXT");
 	if (configStatus > 0) {
-		signalError(true);
+		showError();
 		abort();
 	}
 	
 	byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xF9, 0x83 };
 	int ethernetStatus = Ethernet.begin(mac);
 	if (ethernetStatus > 0) {
-		signalError(true);
+		showError();
 		abort();
 	}
-	  attachInterrupt(0, pairUp, FALLING);   
-	  nss.begin(9600);
-	  nss.println(" ");
-	  nss.println(" ");
-	  nss.println("***** RESET *****");
-	  nss.println(" ");
-		signalError(false);
-		signalAssoc(false);	  
-	  blinkTimer = 0;
-	//coordinator.setData(sendData, sizeof(sendData));
+	
+	attachInterrupt(0, pairUp, FALLING);
+	hideError();
+	hideAssoc();	  
+	blinkTimer = 0;
 }
-
-bool loop_done = false;
 
 void loop(void)
 {
-	//uint8_t ip[4] = {192, 168, 0, 1};
-  //HTTP http(ip, 10);
-  Ethernet.renew();
-  delay(10);
-  uint8_t st = coordinator.tick();
-  uint8_t s = coordinator.getState();
-  assocLed(s);
-  printState(s);
-  if(st == 1 || st == 3) {
-	signalError(true);    
-	//digitalWrite(errorPin, HIGH);
-  }
-  else {
-	signalError(false);
-     //digitalWrite(errorPin, LOW); 
-  }
-  
-  if (!loop_done) {
+	ethernetTick();
+	radioTick();
+}
 
-	Serial.println("Parse start");
-	boolean parse_complete = false;
+void ethernetTick()
+{
+	Ethernet.renew();
+}
 
-	const char *json[] = {"{\"query\": \"from%3Aajstream+%23eg",
-					"ypt\",\"results\": [{\"created_at\": \"Mon, 05 Mar 2012",
-					" 01:00:16 +0000\",\"metadata\": {\"result_type\": \"rece",
-					"nt\"},\"text\": \"Protests at the German University o",
-					"f #Cairo #GUC #Egypt - http://t.co/PeZryCQR\"}],\"results_per_page\": 1}"};
-
-	char * shared_buffer = (char *) calloc(256, sizeof(char));
-	char * text = (char *) calloc(160 + 1, sizeof(char));
-	char * date = (char *) calloc(32 + 1, sizeof(char));
-	TweetParser parser(shared_buffer, text, 160, date, 32);
-	
-	int i = 0;
-	while (i < 5) {
-		memcpy(shared_buffer, json[i], strlen(json[i]));
-		parse_complete = parser.parse(strlen(json[i]));
-		++i;
+void radioTick()
+{
+	uint8_t st = coordinator.tick();
+	uint8_t s = coordinator.getState();
+	assocLed(s);
+	if (st == 1 || st == 3) {
+		showError();
 	}
-
-	Serial.println("Text:");
-	Serial.println(text);
-
-	Serial.println("Date:");
-	Serial.println(date);
-	
-	loop_done = true;
+	else {
+		hideError(); 
 	}
 }
 
@@ -122,12 +82,10 @@ void assocLed(uint8_t state) {
   }
   else {
     if(coordinator.getAssoc()) {
-		signalAssoc(true);      
-		//digitalWrite(assocPin, HIGH);
+		showAssoc();
     }
     else {
-		signalAssoc(false);
-       //digitalWrite(assocPin, LOW);
+		hideAssoc();
     }
   }  
 }
@@ -139,33 +97,31 @@ void pairUp() {
 void blinkPairUp() {
   if(millis() >= blinkTimer) {
 	digitalWrite(assocPin, (assocPinStatus = !assocPinStatus));
-        blinkTimer = millis() + 500;
+    blinkTimer = millis() + 500;
   }
 }
 
-void printState(uint8_t s) {
-  nss.print("State [");
-  nss.print(s);
-  nss.print("]  =  [");
-  nss.print(coordinator.getStateName(s));
-  nss.println("]");
+void showError()
+{
+	digitalWrite(errorPin, HIGH);
 }
 
-void signalError(boolean on) {
-	if (on)
-		digitalWrite(errorPin, HIGH);
-	else
-		digitalWrite(errorPin, LOW);
+void hideError()
+{
+	digitalWrite(errorPin, LOW);
 }
 
-void signalAssoc(boolean on) {
-	if (on)
-		digitalWrite(assocPin, HIGH);
-	else
-		digitalWrite(assocPin, LOW);
+void showAssoc()
+{
+	digitalWrite(assocPin, HIGH);
 }
 
+void hideAssoc()
+{
+	digitalWrite(assocPin, LOW);
+}
+
+// Change this if we implement the watchdog reset timer.
 void abort() {
 	for (;;);
 }
-
