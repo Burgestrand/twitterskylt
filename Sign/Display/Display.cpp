4f8c80fca9a799e857e23d600c686abcdbaca329
Lars Tidstam <larstidstam@gmail.com>
2012-04-22 16:41:00 +0200

    Finish new display library.

--- Sign/Display/Display.cpp
+++ Sign/Display/Display.cpp
@@ -1,5 +1,6 @@
 #include "Display.h"
 
+// TODO: Should be in external file, with rest of the characters.
 static const uint8_t font[] PROGMEM = {
 	/* 0 0x00 '^@' */
 	0x00, 0x00, /* 0000000000 */
@@ -21,7 +22,6 @@ static const uint8_t font[] PROGMEM = {
 	0x00, 0x00, /* 0000000000 */
 	0x00, 0x00, /* 0000000000 */
 
-	/* XXX: Note least significant bytes! */
 	/* 1 0x01 '^A' */
 	0x00, 0x00, /* 0000000000 */
 	0x00, 0x00, /* 0000000000 */
@@ -83,114 +83,91 @@ static const uint8_t font[] PROGMEM = {
 	0x00, 0x00, /* 0000000000 */
 };
 
-#define DISPLAY_SELECT_PIN 10
-#define DISPLAY_BUSY_PIN 8
-#define DISPLAY_RESET_PIN 9
-
 #define CMD_WRITE 0x00
 #define CMD_UPDFULL 0x18
+#define CMD_UPDATE_FULL 0x18
 #define CMD_FILL 0x01
 #define CMD_DARK 0x12
-#define CMD_SLEEP 0x11
+#define CMD_SLEEP 0x20
 
-static inline void disp_wait () {
-	while (digitalRead(DISPLAY_BUSY_PIN)) {}
-}
+#define ROWS_PER_LINE 18
+#define BYTES_PER_ROW 30
+#define FONT_BYTES_PER_ROW 2
+#define FONT_BYTES_PER_CHAR 36
 
 Display::Display(uint8_t cs_pin, uint8_t bsy_pin, uint8_t rst_pin) :
-	cselect_pin(cs_pin), busy_pin(bsy_pin), reset_pin(rst_pin)  {
-		Serial.println("Construcin'");
+	cselect_pin(cs_pin), busy_pin(bsy_pin), reset_pin(rst_pin) {
 }
 
 void Display::begin() {
+	// Configure the SPI interface.
 	SPI.begin();
 	SPI.setDataMode(SPI_MODE1);
 	SPI.setClockDivider(SPI_CLOCK_DIV64);
 	SPI.setBitOrder(MSBFIRST);
 
 	// Configure the pins.
-	pinMode(DISPLAY_BUSY_PIN, INPUT);
-	pinMode(DISPLAY_RESET_PIN, OUTPUT);
+	pinMode(reset_pin, INPUT);
+	pinMode(reset_pin, OUTPUT);
 
 	// Make sure CS is high.
-	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-
-	Serial.println("Pre reset");
+	digitalWrite(cselect_pin, HIGH);
 
 	// Reset the display.
-	digitalWrite(DISPLAY_RESET_PIN, LOW);
-	digitalWrite(DISPLAY_RESET_PIN, HIGH);
-	disp_wait();
-
-	Serial.println("Post reset");
+	reset();
 
 	// Set the display to all black.
-	digitalWrite(DISPLAY_SELECT_PIN, LOW);
-	SPI.transfer(CMD_DARK);
-	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-	disp_wait();
+	black();
 
-	Serial.println("Done initializing");
+	// Set the display to sleep mode to preserve THE POWER!
+	sleep();
 }
 
-void Display::write(char *msgAAA) {
-	Serial.println("Writin'");
-	// Clear ram
-	digitalWrite(DISPLAY_SELECT_PIN, LOW);
-	SPI.transfer(CMD_FILL);
-	SPI.transfer(0);   // START_H
-	SPI.transfer(0);   // START_L
-	SPI.transfer(18);  // END_H
-	SPI.transfer(192); // END_L
-	SPI.transfer(0);   // DATA
-	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-	disp_wait();
+void Display::write(char *msg) {
+	clear_buffer();
 
-	uint8_t msg[] = {2, 4, 3, 1, 1, 3, '\n', 2, 1, 2, 3, 3, 3, 3, '\n', 2, 3, 0};
+	uint8_t line = 0;                      // The line number of the line being written to the display.
+	uint8_t *line_start = (uint8_t *) msg; // The start of the line being written to the display.
+	uint8_t *line_chr;                     // The character being written to the display.
 
-	uint8_t line = 0; 
-	uint8_t *line_start = msg;
-	uint8_t *line_chr;
 	while (true) {
-		uint8_t row = 0;
-		Serial.println("Writin' new line");
-		while (row < 18) {
+		uint8_t row = 0;                   // The pixel row on the line we're going to render.
+		// Write each row to the display.
+		while (row < ROWS_PER_LINE) {
 			line_chr = line_start;
-			uint16_t adr = 30*(row + line*19);
+			write_start(line, row);
 
-			digitalWrite(DISPLAY_SELECT_PIN, LOW);
-			SPI.transfer(CMD_WRITE);
-			SPI.transfer((uint8_t) (adr >> 8));
-			SPI.transfer((uint8_t) adr);
-
+			// Write each character to the display, until row or message ends.
 			while (*line_chr != 0 && *line_chr != '\n') {
+				// Characters has to be sent in pairs, as they are 1.5 bytes wide.
 				uint8_t buf[] = {0, 0, 0};
 				bool two_chars = false;
 
-				buf[0] = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36);
-				buf[1] = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36 + 1);
+				// Buffer the first character.
+				buf[0] = font_data(*line_chr - 1, row, 0);
+				buf[1] = font_data(*line_chr - 1, row, 1);
 
+				// Buffer the second character if there is one.
 				line_chr++;
 				if (*line_chr != 0 && *line_chr != '\n') {
 					two_chars = true;
 
-					buf[1] |= pgm_read_byte_near(font + 2*row + (*line_chr-1)*36)     >> 4;
-					buf[2]  = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36)     << 4;
-					buf[2] |= pgm_read_byte_near(font + 2*row + (*line_chr-1)*36 + 1) >> 4;
+					buf[1] |= font_data(*line_chr - 1, row, 0) >> 4;
+					buf[2]  = font_data(*line_chr - 1, row, 0) << 4;
+					buf[2] |= font_data(*line_chr - 1, row, 1) >> 4;
 
 					line_chr++;
 				}
 
-				SPI.transfer(buf[0]);
-				SPI.transfer(buf[1]);
+				// Send all valid buffer data to the display.
+				send(buf[0]);
+				send(buf[1]);
 				if (two_chars) {
-					SPI.transfer(buf[2]);
+					send(buf[2]);
 				}
-		}
+			}
 			row++;
-
-			digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-			disp_wait();
+			end_cmd();
 		}
 
 		line++;
@@ -204,109 +181,83 @@ void Display::write(char *msgAAA) {
 		}
 	}
 
-	// Update the display
-	digitalWrite(DISPLAY_SELECT_PIN, LOW);
-	SPI.transfer(CMD_UPDFULL);
-	SPI.transfer(0);
-	SPI.transfer(0);
-	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-	disp_wait();
-	Serial.print("Done writin'");
+	// Update the display.
+	update();
+
+	// Set the display to sleep mode to preserve THE POWER!
+	sleep();
 }
 
-
-void doThing() {
-	// Configure SPI
-	SPI.begin();
-	SPI.setDataMode(SPI_MODE1);
-	SPI.setClockDivider(SPI_CLOCK_DIV64);
-	SPI.setBitOrder(MSBFIRST);
-
-	// Configure Pins
-	pinMode(DISPLAY_BUSY_PIN, INPUT);
-	pinMode(DISPLAY_RESET_PIN, OUTPUT);
-
-	// Make sure CS is high
-	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-
-	// Reset display
-	digitalWrite(DISPLAY_RESET_PIN, LOW);
-	digitalWrite(DISPLAY_RESET_PIN, HIGH);
-	disp_wait();
-
-	// Clear ram
-	digitalWrite(DISPLAY_SELECT_PIN, LOW);
-	SPI.transfer(CMD_FILL);
-	SPI.transfer(0);   // START_H
-	SPI.transfer(0);   // START_L
-	SPI.transfer(18);  // END_H
-	SPI.transfer(192); // END_L
-	SPI.transfer(0);   // DATA
-	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-	disp_wait();
-
-	uint8_t msg[] = {2, 4, 3, 1, 1, 3, '\n', 2, 1, 2, 3, 3, 3, 3, 0};
-
-	uint8_t line = 0; 
-	uint8_t *line_start = msg;
-	uint8_t *line_chr;
-	while (true) {
-		uint8_t row = 0;
-		while (row < 18) {
-			line_chr = line_start;
-			uint16_t adr = 30*(row + line*19);
-
-			digitalWrite(DISPLAY_SELECT_PIN, LOW);
-			SPI.transfer(CMD_WRITE);
-			SPI.transfer((uint8_t) (adr >> 8));
-			SPI.transfer((uint8_t) adr);
-
-			while (*line_chr != 0 && *line_chr != '\n') {
-				uint8_t buf[] = {0, 0, 0};
-				bool two_chars = false;
-
-				buf[0] = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36);
-				buf[1] = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36 + 1);
-
-				line_chr++;
-				if (*line_chr != 0 && *line_chr != '\n') {
-					two_chars = true;
-
-					buf[1] |= pgm_read_byte_near(font + 2*row + (*line_chr-1)*36)     >> 4;
-					buf[2]  = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36)     << 4;
-					buf[2] |= pgm_read_byte_near(font + 2*row + (*line_chr-1)*36 + 1) >> 4;
-
-					line_chr++;
-				}
-
-				SPI.transfer(buf[0]);
-				SPI.transfer(buf[1]);
-				if (two_chars) {
-					SPI.transfer(buf[2]);
-				}
-		}
-			row++;
-
-			digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-			disp_wait();
-		}
-
-		line++;
-
-		if (*line_chr == 0) {
-			// All rows of the message has been printed, quit.
-			break;
-		} else {
-			// Skip over the new line character.
-			line_start = line_chr + 1;
-		}
-	}
-
-	// Update the display
-	digitalWrite(DISPLAY_SELECT_PIN, LOW);
-	SPI.transfer(CMD_UPDFULL);
-	SPI.transfer(0);
-	SPI.transfer(0);
-	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
-	disp_wait();
+void Display::clear_buffer() {
+	start_cmd(CMD_FILL);
+	send(0);   // High byte of start of area
+	send(0);   // Low byte
+	send(18);  // High byte of end of area (End = bytes per row*rows = 30*160 = 4800).
+	send(192); // Low byte
+	send(0);   // Data to fill with.
+	end_cmd();
 }
+
+void Display::reset() {
+	digitalWrite(reset_pin, LOW);
+	digitalWrite(reset_pin, HIGH);
+	wait();
+}
+
+void Display::black() {
+	short_cmd(CMD_DARK);
+}
+
+void Display::write_start(uint8_t line, uint8_t row) {
+	start_cmd(CMD_WRITE);
+	uint16_t adr = BYTES_PER_ROW*(row + line*(ROWS_PER_LINE+1)); // +1 for line spacing
+	send((uint8_t) (adr >> 8));
+	send((uint8_t) adr);
+}
+
+void Display::write_data(uint8_t data) {
+	send(data);
+}
+
+void Display::write_end() {
+	end_cmd();
+}
+
+void Display::update() {
+	start_cmd(CMD_UPDATE_FULL);
+	send(0); // High byte of start of image buffer data to be displayed.
+	send(0); // Low byte.
+	end_cmd();
+}
+
+void Display::sleep() {
+	short_cmd(CMD_SLEEP);
+}
+
+void Display::start_cmd(uint8_t cmd) {
+	digitalWrite(cselect_pin, LOW);
+	send(cmd);
+}
+
+void Display::end_cmd() {
+	digitalWrite(cselect_pin, HIGH);
+	wait();
+}
+
+void Display::short_cmd(uint8_t cmd) {
+	start_cmd(cmd);
+	end_cmd();
+}
+
+void Display::wait() {
+	while (digitalRead(busy_pin)) { }
+}
+
+void Display::send(uint8_t data) {
+	SPI.transfer(data);
+}
+
+uint8_t Display::font_data(uint8_t chr, uint8_t row, uint8_t byte) {
+	return pgm_read_byte_near(font + chr*FONT_BYTES_PER_CHAR + row*FONT_BYTES_PER_ROW + byte);
+}
+
--- Sign/Display/Display.h
+++ Sign/Display/Display.h
@@ -6,10 +6,6 @@
 #include <avr/pgmspace.h>
 #include <Arduino.h>
 
-// Setup SPI interface 
-void doThing();
-
-// TODO: The rest
 class Display {
 	public:
 		Display(uint8_t cselect_pin, uint8_t busy_pin, uint8_t reset_pin);
@@ -22,19 +18,50 @@ class Display {
 		uint8_t busy_pin;
 		uint8_t reset_pin;
 
+		// Reset the display.
 		void reset();
+
+		// Turn the display completely dark.
 		void black();
+
+		// Start writing a row of pixels for the row:th row of line line.
+		// Clears old data from buffer.
 		void write_start(uint8_t line, uint8_t row);
-		void write_data();
+
+		// Data for the row. Don't write more than 30 bytes!
+		void write_data(uint8_t data);
+
+		// Finish writing row.
 		void write_end();
-		void update();
+
+		// Helpers
+		// Clears image buffer.
+		void clear_buffer();
+
+		// Set display to sleep mode.
 		void sleep();
 
-		void csl();
-		void csh();
+		// Update the (whole) display.
+		void update();
+
+		// Start a command with arguments.
+		void start_cmd(uint8_t cmd);
+
+		// End a command with arguments.
+		void end_cmd();
+
+		// Send a command without arguments.
+		void short_cmd(uint8_t cmd);
+
+		// Wait for the display to become non-busy.
 		void wait();
-		void send();
-		void startcmd(uint8_t cmd);
+
+		// Send a byte to the display.
+		void send(uint8_t data);
+
+		// Get the offset for a row of a character.
+		// byte controls whether 0:th or 1:st byte is returned.
+		uint8_t font_data(uint8_t chr, uint8_t row, uint8_t byte);
 };
 
 #endif
