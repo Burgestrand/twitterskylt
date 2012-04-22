a8e150dcdace0deb300aa2c8620d6b1161bfe7e4
Lars Tidstam <larstidstam@gmail.com>
2012-04-22 13:36:05 +0200

    Remove old Display library, add start of new library.

--- Sign/Display/Display.cpp
+++ Sign/Display/Display.cpp
@@ -1,65 +1,192 @@
-/* Display handling code, for displaying text on the display
- */
-
-/* TODO:
- * Inherit print class for print function
- */
-
-#include "Arduino.h"
 #include "Display.h"
 
+static const uint8_t font[] PROGMEM = {
+	/* 0 0x00 '^@' */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
 
-// Constructor defining the pins used by the display.
-Display::Display(uint8_t rs, uint8_t e, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) : _disp(rs, e, d4, d5, d6, d7) {
-	_row = 0;
-	_done = 0;
+	/* XXX: Note least significant bytes! */
+	/* 1 0x01 '^A' */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x3f, 0x80, /* 0011111110 */
+	0x40, 0x40, /* 0100000001 */
+	0x5b, 0x40, /* 0101101101 */
+	0x40, 0x40, /* 0100000001 */
+	0x44, 0x40, /* 0100010001 */
+	0x44, 0x40, /* 0100010001 */
+	0x51, 0x40, /* 0101000101 */
+	0x4e, 0x40, /* 0100111001 */
+	0x40, 0x40, /* 0100000001 */
+	0x3f, 0x80, /* 0011111110 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+
+	/* 2 0x02 '^B' */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x3f, 0x80, /* 0011111110 */
+	0x7f, 0xc0, /* 0111111111 */
+	0x64, 0xc0, /* 0110010011 */
+	0x7f, 0xc0, /* 0111111111 */
+	0x7b, 0xc0, /* 0111101111 */
+	0x7b, 0xc0, /* 0111101111 */
+	0x6e, 0xc0, /* 0110111011 */
+	0x71, 0xc0, /* 0111000111 */
+	0x7f, 0xc0, /* 0111111111 */
+	0x3f, 0x80, /* 0011111110 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+
+	/* 3 0x03 '^C' */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x11, 0x00, /* 0001000100 */
+	0x3b, 0x80, /* 0011101110 */
+	0x7f, 0xc0, /* 0111111111 */
+	0x3f, 0x80, /* 0011111110 */
+	0x3f, 0x80, /* 0011111110 */
+	0x1f, 0x00, /* 0001111100 */
+	0x1f, 0x00, /* 0001111100 */
+	0x0e, 0x00, /* 0000111000 */
+	0x0e, 0x00, /* 0000111000 */
+	0x04, 0x00, /* 0000010000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+	0x00, 0x00, /* 0000000000 */
+};
+
+#define DISPLAY_SELECT_PIN 10
+#define DISPLAY_BUSY_PIN 8
+#define DISPLAY_RESET_PIN 9
+
+#define CMD_WRITE 0x00
+#define CMD_UPDFULL 0x18
+#define CMD_FILL 0x01
+
+static inline void disp_wait () {
+	while (digitalRead(DISPLAY_BUSY_PIN)) {}
 }
 
-// Initialize and clear display.
-void Display::begin(uint8_t col, uint8_t row) {
-	_disp.begin(col, row);
-	_disp.clear();
-}
+void doThing() {
+	// Configure SPI
+	SPI.begin();
+	SPI.setDataMode(SPI_MODE1);
+	SPI.setClockDivider(SPI_CLOCK_DIV64);
+	SPI.setBitOrder(MSBFIRST);
 
-// Set the cursor to the start of the next line, wraps around to the first line if needed.
-void Display::new_line() {
-	_row = (_row + 1) % 2; // XXX: Hard coded number of lines!
-	_disp.setCursor(0, _row);
-}
+	// Configure Pins
+	pinMode(DISPLAY_BUSY_PIN, INPUT);
+	pinMode(DISPLAY_RESET_PIN, OUTPUT);
 
-// Write a char, handles newlines and end of message characters
-// If a end of message character has been encountered, the next 
-// time a char is printed the display is cleared before outputting it.
-void Display::write(uint8_t c) {
-	if (_done) {
-		clear();
-	}
-	switch (c) {
-		case '\004':
-			_done=1;
+	// Make sure CS is high
+	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
+
+	// Reset display
+	digitalWrite(DISPLAY_RESET_PIN, LOW);
+	digitalWrite(DISPLAY_RESET_PIN, HIGH);
+	disp_wait();
+
+	// Clear ram
+	digitalWrite(DISPLAY_SELECT_PIN, LOW);
+	SPI.transfer(CMD_FILL);
+	SPI.transfer(0);   // START_H
+	SPI.transfer(0);   // START_L
+	SPI.transfer(18);  // END_H
+	SPI.transfer(192); // END_L
+	SPI.transfer(0);   // DATA
+	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
+	disp_wait();
+
+	uint8_t msg[] = {2, 4, 3, 1, 1, 3, '\n', 2, 1, 2, 3, 3, 3, 3, 0};
+
+	uint8_t line = 0; 
+	uint8_t *line_start = msg;
+	uint8_t *line_chr;
+	while (true) {
+		uint8_t row = 0;
+		while (row < 18) {
+			line_chr = line_start;
+			uint16_t adr = 30*(row + line*19);
+
+			digitalWrite(DISPLAY_SELECT_PIN, LOW);
+			SPI.transfer(CMD_WRITE);
+			SPI.transfer((uint8_t) (adr >> 8));
+			SPI.transfer((uint8_t) adr);
+
+			while (*line_chr != 0 && *line_chr != '\n') {
+				uint8_t buf[] = {0, 0, 0};
+				bool two_chars = false;
+
+				buf[0] = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36);
+				buf[1] = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36 + 1);
+
+				line_chr++;
+				if (*line_chr != 0 && *line_chr != '\n') {
+					two_chars = true;
+
+					buf[1] |= pgm_read_byte_near(font + 2*row + (*line_chr-1)*36)     >> 4;
+					buf[2]  = pgm_read_byte_near(font + 2*row + (*line_chr-1)*36)     << 4;
+					buf[2] |= pgm_read_byte_near(font + 2*row + (*line_chr-1)*36 + 1) >> 4;
+
+					line_chr++;
+				}
+
+				SPI.transfer(buf[0]);
+				SPI.transfer(buf[1]);
+				if (two_chars) {
+					SPI.transfer(buf[2]);
+				}
+		}
+			row++;
+
+			digitalWrite(DISPLAY_SELECT_PIN, HIGH);
+			disp_wait();
+		}
+
+		line++;
+
+		if (*line_chr == 0) {
+			// All rows of the message has been printed, quit.
 			break;
-		case '\n':
-			new_line();
-			_done=0;
-			break;
-		default:
-			_disp.write(c);
-			_done=0;
-			break;
+		} else {
+			// Skip over the new line character.
+			line_start = line_chr + 1;
+		}
 	}
-}
 
-// Null terminate the string, or else!
-void Display::write(char * msg) {
-	while (*msg != '\0') {
-		write(*msg);
-		msg++;
-	}
+	// Update the display
+	digitalWrite(DISPLAY_SELECT_PIN, LOW);
+	SPI.transfer(CMD_UPDFULL);
+	SPI.transfer(0);
+	SPI.transfer(0);
+	digitalWrite(DISPLAY_SELECT_PIN, HIGH);
+	disp_wait();
 }
-
-// Clear the display.
-void Display::clear() {
-	_disp.clear();
-	_row = 0;
-}
-
--- Sign/Display/Display.h
+++ Sign/Display/Display.h
@@ -1,47 +1,11 @@
-/* Display handling code, for displaying text on the display
- *
- * Create display instance with enable, rs and data pins
- * use write/newline/clear to display data on the display
- */
+/* Module for communicating with the display */
+#ifndef DISPLAY
+#define DISPLAY
 
-#ifndef Display_h
-#define Display_h
+#include <SPI.h>
+#include <avr/pgmspace.h>
 
-#include <Arduino.h>
-#include <LiquidCrystal.h>
-
-class Display {
-	public:
-		// Define the pins used with the display.
-		Display(uint8_t rs, uint8_t e, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
-
-		// Initialize and clear the display.
-		void begin(uint8_t col, uint8_t row);
-
-		// Set the cursor the the start of the next line, wraps around to the top line if needed.
-		void new_line();
-
-		// Clear the display.
-		void clear();
-
-		// Write a char to the display. Newlines are handled.
-		// If a end of message character ('\004') is encountered the display is cleared before
-		// the next character is written.
-		void write(uint8_t c);
-
-		// Writes each charater to the display by repeatedly calling write(uint8_t) until a null
-		// byte is encountered.
-		void write(char * msg);
-	private:
-		// The library used to communicate with the display.
-		LiquidCrystal _disp;
-
-		// Current row.
-		uint8_t _row;
-
-		// True if an end of message character has been encountered, causes the next write to
-		// clear the screen before it is written.
-		uint8_t _done;
-};
+// Setup SPI interface 
+void doThing();
 
 #endif
