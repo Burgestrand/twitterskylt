aab18179592cbe2a7ea126e023d1bd689476be30
Lars Tidstam <larstidstam@gmail.com>
2012-04-25 23:27:42 +0200

    Add font with built in spacing.

---
+++ Sign/Display/fontwide.h
@@ -0,0 +1,11 @@
+/********************************
+ * adapted from font_sun12x22.c *
+ * by Jurriaan Kalkman 06-2005  *
+ ********************************/
+
+#include <avr/pgmspace.h>
+
+#ifndef Display_fontwide_h
+#define Display_fontwide_h
+extern const uint8_t PROGMEM font[];
+#endif
