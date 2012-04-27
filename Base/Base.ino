bcfcbccbf72d609f8a69e314150e57cd22896f73
andreas <ake.andreas@gmail.com>
2012-04-27 14:42:22 +0200

    added code for parsing

--- Base/Base.ino
+++ Base/Base.ino
@@ -16,6 +16,8 @@
 #include <Ethernet.h>
 #include <AccConfig.h>
 #include <SD.h>
+#include <TweetParser.h>
+#include <MemoryFree.h>
 
 Coordinator coordinator;
 
@@ -31,27 +33,33 @@ void setup(void)
 	Serial.begin(9600);
 	pinMode(errorPin, OUTPUT);
 	pinMode(assocPin, OUTPUT);
+	pinMode(53, OUTPUT);
 	coordinator.begin(Serial1);
-
+	pinMode(10, OUTPUT);
+	pinMode(4, OUTPUT);
+	digitalWrite(10, HIGH);
 	int configStatus = config.begin("KONF1.TXT");
 	if (configStatus > 0) {
+		Serial.println("SD fail");
 		showError();
 		abort();
 	}
-
+	digitalWrite(4, HIGH);
 	byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xF9, 0x83 };
 	int ethernetStatus = Ethernet.begin(mac);
 	if (ethernetStatus > 0) {
+		Serial.println("eth fail");
 		showError();
 		abort();
 	}
-
+	
+	Serial.println("everything ok!");
 	attachInterrupt(0, pairUp, FALLING);
 	hideError();
 	hideAssoc();
 	blinkTimer = 0;
 
-	setupTimezone();
+	//setupTimezone();
 }
 
 void setupTimezone()
@@ -60,7 +68,7 @@ void setupTimezone()
   HTTP *client = new HTTP(buffer_size);
 
   Serial.println("Get: ");
-  int get = client->get(IPAddress(199, 59, 150, 9), "/1/users/show.json", 2, "screen_name", "Burgestrand");
+  int get = client->get(IPAddress(199, 59, 150, 9), "/1/users/show.json", 2, "screen_name", "door_sign");
 
   if (get == 0)
   {
@@ -100,11 +108,11 @@ void setupTimezone()
 
 void loop(void)
 {
-	//ethernetTick();
-	//radioTick();
+	ethernetTick();
+	radioTick();
 	Serial.println("loop");
-  delay(5000);
-	//tweetTick();
+	tweetTick();
+	delay(1000);
 }
 
 void ethernetTick()
@@ -125,42 +133,58 @@ void radioTick()
 	}
 }
 
-//TweetParser parser = NULL;
 HTTP *httpClient = NULL;
 unsigned long nextRequest = 0;
 unsigned long updateInterval = 60 * 1000; // 1 minute interval
 
-#define HTTP_BUFFER_SIZE 1024
+#define HTTP_BUFFER_SIZE 200
 
 void tweetTick()
 {
+	char * text;
+	char * date;
   if (millis() >= nextRequest)
   {
+	text = (char *) calloc(161, sizeof(char));
+	date = (char *) calloc(41, sizeof(char));
+	TweetParser parser = TweetParser(text, 160, date, 40);
+	Serial.println("lets get");
     // calculate next time to do the request
     nextRequest += updateInterval;
     // httpClient.~HTTP();
     httpClient = new HTTP(HTTP_BUFFER_SIZE);
     // new request
+	int8_t ret = httpClient->get(IPAddress(199,59,148,201), "/search.json", 6, "q", "from:door_sign", "result_type", "recent", "rpp", "1");
+	int32_t length = 0;
+	bool finished = false;
+	while (!finished) {
+		const char * buffer = httpClient->tick(&length);
+		if (length > 0) {
+			fixJSON(buffer);
+			finished = parser.parse(buffer, length);
+		}
+	}
+	httpClient->rem();
+	parser.del();
+	char * result = Formatting::format(text,date,3600);
+	coordinator.setData((uint8_t *)result, (uint8_t)strlen(result));
+	Serial.println(result);
   }
   else
   {
-
+		Serial.println("no");
   }
+}
 
-  /*
-	#define UPDATE_INTERVAL 10000
-	char *buffer = (char *) calloc(SIZE, sizeof(char));
-	char *text = (char *) calloc(161, sizeof(char));
-	char *date = (char *) calloc(41, sizeof(char));
-
-	TweetParser parser = TweetParser(buffer, text, 161, date, 40);
-
-  lastRequest = millis();
-
-  char * result = Formatting::format(text, date, 0);
-  Serial.println("formatted tweet:");
-  Serial.println(result);
-  */
+// removes \ to pass yajl
+void fixJSON(const char * str) {
+	char * ptr = (char *) str;
+	while(*ptr) {
+		if (*ptr == '\\') {
+			*ptr = '/';
+		}
+		ptr++;
+	}
 }
 
 void assocLed(uint8_t state) {
