74efb43ad8a269ca295dff3d7cb1b398ecc79d8b
Fredrik Brosser <fb.msngr@gmail.com>
2012-05-04 11:11:56 +0200

    Modify sketch for production version of sign

--- Sign/Sign.ino
+++ Sign/Sign.ino
@@ -12,6 +12,12 @@
 // Debouncing update/join buttons?
 // Radio error handling
 
+// Constants related to battery level reading
+#define V_RES 0.004882814
+#define V_TH 0.7
+#define V_LOW 3.3
+#define Q_DROP 408
+
 // Pins
 // Display related
 #define DISPLAY_SELECT_PIN 10
@@ -21,18 +27,22 @@
 // Software serial pins for debugging
 #define SERIAL_RX 7
 #define SERIAL_TX 6
+// Join Button
+#define JOIN_BUTTON 2
+// Update Button
+#define UPDATE_BUTTON 3
+// Red LED
+#define ERROR_LED 6
+// Yellow LED
+#define BATTERY_LED 5
+// Green LED
+#define ASSOC_LED 4
 
-// Leds
-// The red led
-#define LED_1 A0
-// The yellow led
-#define LED_2 A1
+// Analog battery reading
+#define BATT_A 0
+//Voltage divider control
+#define VDIV_D 7
 
-// Buttons
-// The button closest to the leds
-#define BUTTON_1 2
-// The button furthest from the leds
-#define BUTTON_2 3
 
 // XBee sleep request/status
 #define SLEEP_RQ_PIN 4
@@ -54,6 +64,11 @@ volatile bool joinPressed = false;
 // True if the update button has been pressed, but not yet handled
 volatile bool updatePressed = false;
 
+// True if the battery level is low
+bool lowBattery = false;
+// Analog value read from voltage divider
+float batteryLevel = 0;
+
 // Helpers
 // Debug output callback
 void debug(char *msg) {
@@ -72,12 +87,30 @@ void updateButtonPressed(void) {
   updatePressed = true;
 }
 
+float getPinVoltage(uint8_t pin) {
+	return (analogRead(pin) * V_RES);
+}
+
+float getBatteryVoltage(uint8_t pin) {
+  // Voltage divider divides voltage by 2
+  // Value from analog port has resolution of 49mV (5V/1024)
+  // Also adjusts for voltage drop across MOSFET (approximation)
+  int batteryLevel = analogRead(0);
+  float Vbatt = (2*batteryLevel*V_RES) - 0.04*(batteryLevel/Q_DROP);
+  return Vbatt;
+}
+
 // Usual arduino functions
 void setup () {
-  // Activity  led indicating that the arduino is awake
-  pinMode(LED_1, OUTPUT);
-  digitalWrite(LED_1, HIGH);
-  
+  pinMode(ASSOC_LED, OUTPUT);
+  digitalWrite(ASSOC_LED, LOW);
+  pinMode(ERROR_LED, OUTPUT);
+  digitalWrite(ERROR_LED, LOW);
+  pinMode(BATTERY_LED, OUTPUT);
+  digitalWrite(ERROR_LED, LOW);
+
+  pinMode(VDIV_D, OUTPUT);
+  digitalWrite(VDIV_D, LOW);
   // Initialize the display
   disp.begin();  
   
@@ -107,28 +140,39 @@ void loop () {
     disp.write("Trying to join\nnetwork");
     radio.joinNetwork();
   }
-
+  
+  digitalWrite(VDIV_D, HIGH);
+  batteryLevel = getBatteryVoltage(BATT_A);
+  delay(1);
+  digitalWrite(BATTERY_LED, (batteryLevel <= V_LOW ? HIGH : LOW));
+  
   // Necessary for the radio library to work
   switch (radio.tick()) {
     case TICK_ASSOC_FAIL:
       // Association failed (note: not pairing)
       disp.write("Assoc fail");
+      digitalWrite(ERROR_LED, HIGH);
       break;
     case TICK_JOIN_NOT_DELIVERED:
       // Could not deliver join message
       disp.write("Join:\n No delivery");
+      digitalWrite(ERROR_LED, HIGH);
       break;
     case TICK_JOIN_TIMEOUT:
       disp.write("Join:\n Timeout");
+      digitalWrite(ERROR_LED, HIGH);
       break;
     case TICK_JOIN_OK:
       disp.write("Joined!");
+      digitalWrite(ASSOC_LED, HIGH);
       break;
     case TICK_UPDATE_NO_DELIVERY:
       disp.write("Update:\n No delivery");
+      digitalWrite(ERROR_LED, HIGH);
       break;
     case TICK_UPDATE_TIMEOUT:
       disp.write("Update:\n Timeout");
+      digitalWrite(ERROR_LED, HIGH);
       break;
     case TICK_NEW_MSG:
       disp.write((char *) radio.getData());
@@ -136,7 +180,6 @@ void loop () {
       break;
     case TICK_SLEEPING:
       // Radio is sleeping and we have nothing to do; lets sleep!
-      digitalWrite(LED_1, LOW);
       {
         uint8_t sleepRounds = 3;
         // Sleep until we've sleept long enough or a button has been pressed.
@@ -146,14 +189,17 @@ void loop () {
       }
       // If the join button was pressed we shouldn't update the message.
       if (!joinPressed) {
-        digitalWrite(LED_1, HIGH);
         radio.wakeup();
         radio.getNewestMessage();
       }
       updatePressed = false;
       break;
     case TICK_UNKNOWN_ERROR:
+      digitalWrite(ERROR_LED, HIGH);
+      break;
     case TICK_JOIN_BAD_MSG:
+      digitalWrite(ERROR_LED, HIGH);
+      break;
     case TICK_OK:
     default:
       // Do nothing
